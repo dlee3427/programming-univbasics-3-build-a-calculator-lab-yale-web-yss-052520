beed66a472752e756923b1417ff984d83677ced3
David Lee <david.lee.sl2634@yale.edu>
2020-05-09 19:55:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,35 +1,35 @@
 def addition(num1, num2)
-  result = num1 + num2
+  result = num1.to_i + num2.to_i
   puts result 
   result
 end
 
 def subtraction(num1, num2)
-  result = num2 - num1 
+  result = num2.to_i - num1.to_i 
   puts result 
   result
 end
 
 def division(num1, num2)
-  result2 = num2 / num1 
+  result2 = num2.to_i / num1.to_i 
   puts result2
-  result 2
+  result2
 end
 
 def multiplication(num1, num2)
-  result3 = num1 * num2 
+  result3 = num1.to_i * num2.to_i 
   puts result3
   result3
 end
 
 def modulo(num1, num2)
-  result4 = num1 % num1 
-  puts result4
+  remainder = num1.to_i % num2.to_i 
+  puts remainder
   result4
 end
 
 def square_root(num)
-  root = Math.sqrt(num)
+  root = Math.sqrt(num.to_i)
   puts root
   root
 end
